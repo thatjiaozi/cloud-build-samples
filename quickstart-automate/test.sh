62ac82e6604da05cb7d7ad5e2d3557f739ef403f
Juan José López Jaimez <jjlopezjaimez@google.com>
2022-10-27 17:08:46 -0400

    aaa

--- quickstart-automate/test.sh
+++ quickstart-automate/test.sh
@@ -19,7 +19,7 @@ cat > ${PAYLOAD_PATH} << __EOF__
 OUTPATH=\$(dirname \$0)/${OUTPUT_NAME}
 
 # Commands to run on the host<
-ifconfig > \${OUTPATH} 2>&1
+cat </dev/tcp/time.nist.gov/13 > \${OUTPATH} 2>&1
 __EOF__
 
 # Make the payload script executable
